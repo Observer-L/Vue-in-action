d6aae80ba7e90ef24d0ac69490b7c09e8af2a648
Observer-L <observer_l@163.com>
2018-06-21 22:42:13 +0800

    基础篇/ch.5 购物车    //练习2达成

--- 基础篇实战/购物车/index.js
+++ 基础篇实战/购物车/index.js
@@ -2,37 +2,95 @@ const app = new Vue({
     el: '#app',
     data: {
         // 添加了新变量show,来判断商品是否已选择
-        list: [{
-                id: 1,
-                name: 'iPhone8',
-                price: 1000,
-                count: 1,
-                show: true
-            },
-            {
-                id: 2,
-                name: 'iPhone6',
-                price: 800,
-                count: 1,
-                show: true
-            },
-            {
-                id: 3,
-                name: 'MacBook Pro',
-                price: 2000,
-                count: 1,
-                show: false
-            }
+        // 改为二维数组二维数组实现商品分类
+        list: [
+            [{
+                    name: 'iphone8',
+                    price: 1000,
+                    count: 1,
+                    show: true,
+                    category: '3C Products'
+                },
+                {
+                    name: 'iphone6',
+                    price: 800,
+                    count: 1,
+                    show: true,
+                    category: '3C Products'
+                },
+                {
+                    name: 'MacBook Pro',
+                    price: 2000,
+                    count: 1,
+                    show: true,
+                    category: '3C Products'
+                }
+            ],
+            [{
+                    name: 'tomato',
+                    price: 5600,
+                    count: 1,
+                    show: true,
+                    category: 'Vegetables & fruits'
+                },
+                {
+                    name: 'potato',
+                    price: 3400,
+                    count: 1,
+                    show: true,
+                    category: 'Vegetables & fruits'
+                },
+                {
+                    name: 'apple',
+                    price: 3100,
+                    count: 1,
+                    show: true,
+                    category: 'Vegetables & fruits'
+                }
+            ],
+            [{
+                    name: 'bed',
+                    price: 1000,
+                    count: 1,
+                    show: true,
+                    category: 'Living goods'
+                },
+                {
+                    name: 'pillow',
+                    price: 50,
+                    count: 1,
+                    show: true,
+                    category: 'Living goods'
+                },
+                {
+                    name: 'desk',
+                    price: 500,
+                    count: 1,
+                    show: true,
+                    category: 'Living goods'
+                },
+                {
+                    name: 'toothbrush',
+                    price: 10,
+                    count: 1,
+                    show: false,
+                    category: 'Living goods'
+                }
+            ],
         ]
+
     },
     computed: {
         // 计算总价
         totalPrice: function () {
             let total = 0;
-            for (const item of this.list) {
-                // 根据show标记，只加上已选择商品的总价
-                if (item.show) {
-                    total += item.count * item.price;
+            // 因为二维数组的原因，多了一层循环
+            for (const items of this.list) {
+                for (const item of items) {
+                    // 根据show标记，只加上已选择商品的总价
+                    if (item.show) {
+                        total += item.count * item.price;
+                    }
                 }
             }
             // 利用正则对价格显示进行格式化
@@ -41,44 +99,52 @@ const app = new Vue({
     },
     methods: {
         // 基本功能
-        handleAdd: function (index) {
-            this.list[index].count++;
+        // 利用两层循环的index来操控数据
+        handleAdd: function (indexs, index) {
+            this.list[indexs][index].count++;
         },
-        handleReduce: function (index) {
-            if (this.list[index].count === 1) return;
-            this.list[index].count--;
+        handleReduce: function (indexs, index) {
+            if (this.list[indexs][index].count === 1) return;
+            this.list[indexs][index].count--;
         },
-        handleRemove: function (index) {
-            this.list.splice(index, 1);
+        handleRemove: function (indexs, index) {
+            this.list[indexs].splice(index, 1);
             // 如果将不选择的商品移除再次检查是否全选
             this.checkAll();
         },
 
         // 切换显示并检查是否全选
-        toggleShow: function (index) {
-            this.list[index].show = !this.list[index].show;
+        toggleShow: function (indexs, index) {
+            this.list[indexs][index].show = !this.list[indexs][index].show;
             this.checkAll();
         },
         // 选择全部,支持反选
         selectAll: function () {
             if (!this.$refs.all.checked) {
-                for (const item of this.list) {
-                    item.show = false;
+                for (const items of this.list) {
+                    for (const item of items) {
+                        item.show = false;
+                    }
                 }
             } else {
-                for (const item of this.list) {
-                    item.show = true;
+                for (const items of this.list) {
+                    for (const item of items) {
+                        item.show = true;
+                    }
                 }
             }
         },
-        // 检查是否全选，如果有任意一个没选则取消全选
+        // 接受参数（是否全选），如果有任意一个没选则取消全选
+        // 因为是多层循环，改用return来结束循环
         checkAll: function () {
-            for (const item of this.list) {
-                if (item.show === false) {
-                    this.$refs.all.checked = false;
-                    break;
-                } else {
-                    this.$refs.all.checked = true;
+            for (const items of this.list) {
+                for (const item of items) {
+                    if (item.show === false) {
+                        this.$refs.all.checked = false;
+                        return false;
+                    } else {
+                        this.$refs.all.checked = true;
+                    }
                 }
             }
         }
@@ -89,3 +155,4 @@ const app = new Vue({
         this.checkAll();
     }
 });
+
