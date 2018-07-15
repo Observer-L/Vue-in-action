1848a4ca0add84a65be07376f44197d881398f42
Observer-L <observer_l@163.com>
2018-07-15 16:17:02 +0800

    进阶篇/ch.9 可排序表格 // 基本实现

---
+++ 进阶篇实战/可排序表格/index.js
@@ -0,0 +1,66 @@
+const app = new Vue({
+    el: '#app',
+    /* columns的每一项是一个对象
+    title和key用来标识列的表头标题，必填。title对应组件data中列内容的字段名，key用来索引行内容
+    sortable是选填字段，true为需要排序
+    */
+    data: {
+        columns: [{
+                title: '姓名',
+                key: 'name',
+                sortable: false
+            },
+            {
+                title: '年龄',
+                key: 'age',
+                sortable: true
+            },
+            {
+                title: '出生日期',
+                key: 'birthday',
+                sortable: true
+            },
+            {
+                title: '地址',
+                key: 'address',
+                sortable: false
+            }
+        ],
+        data: [{
+                name: '王小明',
+                age: 18,
+                birthday: '1999-02-21',
+                address: '北京市朝阳区芍药居'
+            },
+            {
+                name: '张小刚',
+                age: 25,
+                birthday: '1992-01-23',
+                address: '北京市海淀区西二旗'
+            },
+            {
+                name: '李小红',
+                age: 30,
+                birthday: '1987-11-10',
+                address: '上海市浦东新区世纪大道'
+            },
+            {
+                name: '周小伟',
+                age: 26,
+                birthday: '1991-10-10',
+                address: '深圳市南山区深南大道'
+            }
+        ],
+        value: 50
+    },
+    methods: {
+        handleAddData: function () {
+            this.data.push({
+                name: '刘小天',
+                age: 19,
+                birthday: '1998-5-30',
+                address: '北京市东城区东直门'
+            });
+        }
+    }
+});
---
+++ 进阶篇实战/可排序表格/table.js
@@ -0,0 +1,141 @@
+Vue.component('vTable', {
+    props: {
+        columns: {
+            type: Array,
+            default: function () {
+                return [];
+            }
+        },
+        data: {
+            type: Array,
+            default: function () {
+                return [];
+            }
+        }
+    },
+    // 让原始数据和排序后的数据互相独立
+    // 需要在初始化的时候赋值过去
+    data: function () {
+        return {
+            currentColumns: [],
+            currentData: []
+        }
+    },
+    render: function (h) {
+        const _this = this;
+        const ths = [];
+        const trs = [];
+        // 表头
+        this.currentColumns.forEach(function (col, index) {
+            if (col.sortable) {
+                ths.push(h('th', [
+                    h('span', col.title),
+                    // 升序
+                    h('a', {
+                        // 根据当前的排序状态设置样式
+                        class: {
+                            on: col._sortType === 'asc'
+                        },
+                        on: {
+                            click: function () {
+                                _this.handleSort(index, true)
+                            }
+                        }
+                    }, '↑'),
+                    // 降序
+                    h('a', {
+                        class: {
+                            on: col._sortType === 'desc'
+                        },
+                        on: {
+                            click: function () {
+                                _this.handleSort(index, false)
+                            }
+                        }
+                    }, '↓')
+                ]));
+            } else {
+                // 无需排序则直接打标题
+                ths.push(h('th', col.title));
+            }
+        })
+        // 行内容
+        this.currentData.forEach(function (row) {
+            const tds = [];
+            _this.currentColumns.forEach(function (cell) {
+                tds.push(h('td', row[cell.key]));
+            });
+            trs.push(h('tr', tds));
+        })
+        // 返回组合好的行内容和表头节点进行渲染
+        return h('table', [
+            h('thead', [
+                h('tr', ths)
+            ]),
+            h('tbody', trs)
+        ])
+    },
+    methods: {
+        // 用来给data在初始化时赋值的函数
+        makeColumns: function () {
+            this.currentColumns = this.columns.map(function (col, index) {
+                // 添加一个字段标识当前列排序的状态
+                // 只能同时对一列数据进行排序，即列与列之间的排序是互斥的，所以需要标记当前列的排序状态（默认normal不排序）
+                col._sortType = 'normal';
+                // 添加一个字段标识当前列在数组中的索引
+                col._index = index;
+                return col;
+            });
+        },
+        makeData: function () {
+            this.currentData = this.data.map(function (row, index) {
+                // 添加一个字段标识当前行在数组中的索引
+                // 因为排序后currentData中数据顺序被打乱，所以需要标识每一项在原始数据中的索引。
+                row._index = index;
+                return row;
+            })
+        },
+        // 排序函数，整合升降排序函数。
+        // 第二个参数isAsc为是否升序，默认降序。
+        handleSort: function (index, isAsc) {
+            const key = this.currentColumns[index].key;
+            this.currentColumns.forEach(function (col) {
+                col._sortType = 'normal';
+            });
+            if (isAsc) {
+                this.currentColumns[index]._sortType = 'asc';
+                this.currentData.sort(function (a, b) {
+                    return a[key] > b[key] ? 1 : -1;
+                });
+            } else {
+                this.currentColumns[index]._sortType = 'desc';
+                this.currentData.sort(function (a, b) {
+                    return a[key] < b[key] ? 1 : -1;
+                });
+            }
+
+        }
+    },
+    mounted() {
+        // v-table 初始化时调用
+        this.makeColumns();
+        this.makeData();
+    },
+    watch: {
+        // 对父级的data数据进行监听
+        data: function () {
+            this.makeData();
+            // 如果某行已经处于排序状态，那么更新（删减行）后应该再处理一次排序
+            const sortedColumn = this.currentColumns.filter(function (col) {
+                return col._sortType !== 'normal';
+            });
+            if (sortedColumn.length > 0) {
+                if (sortedColumn[0]._sortType === 'asc') {
+                    this.handleSort(sortedColumn[0]._index, true);
+                } else {
+                    this.handleSort(sortedColumn[0]._index, false);
+                }
+            }
+        }
+    }
+})
